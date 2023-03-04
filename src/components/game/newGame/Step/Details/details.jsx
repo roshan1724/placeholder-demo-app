b8204dad6f549d6c7fb81d2dc149f92df18ac10a
roshan1724 <kumar1724roshan@gmail.com>
2023-03-05 00:59:03 +0530

    Game flow added

--- src/components/game/newGame/Step/Details/details.jsx
+++ src/components/game/newGame/Step/Details/details.jsx
@@ -9,7 +9,7 @@ function GameDetails () {
 
   const formOptionsData = {
     start_time: {
-      hours: Array.from({length: 24}, (v, k) => k + 1),
+      hours: Array.from({length: 12}, (v, k) => k + 1),
       minutes: Array.from({length: 60}, (v, k) => k + 1),
       meredian: ['AM', 'PM']
     },
@@ -17,7 +17,14 @@ function GameDetails () {
   }
 
   const handleDateChange = (e) => {
-    gameDetailForm.values.start_date = e.target.value;
+    console.log(e.target.value);
+    // const dateElement = document.getElementById('game-detail-date');
+    // dateElement.value = e.target.value;
+    gameDetailForm.setFieldValue('start_date', e.target.value);
+    // gameDetailForm.handleChange(e);
+
+    // console.log(gameDetailForm.values);
+    // gameDetailForm.values.start_date = e.target.value;
   }
 
   const handleBackClick = () => {
@@ -30,40 +37,200 @@ function GameDetails () {
 
   const handleDateIconClick = () => {
     const element = document.getElementById('date-copy');
-    console.log('Selected Element =>', element)
     element.showPicker();
   }
 
   const gameDetailForm = GameDetailForm(formOptionsData);
+  console.log(gameDetailForm.values);
 
   return (
     <section className="section-game-scenario">
       <div className="detail-form-wrapper">
         <form className="game-detail-form">
-          <div className="field-group-wrapper">
-            <p className="field-label">Schedule the game start date and time</p>
-            <div className="field-group-content">
-              <div className="custom-form-block">
-                <label htmlFor="game-detail-date" className="form-label">
-                  Date
-                </label>
-                <div className="input-group date-field has-validation">
-                  <input type="date" name="test" id="date-copy" onChange={handleDateChange} />
-                  <input
-                    type="text" 
-                    name="start_date"
-                    id='game-detail-date'
-                    className={`form-control ${(gameDetailForm.touched.start_date && gameDetailForm.errors.start_date) ? 'is-invalid' : ''}`}
-                    placeholder='Select Date'
-                    onChange={gameDetailForm.handleChange}
-                    onBlur={gameDetailForm.handleBlur}
-                    value={gameDetailForm.values.start_date}/>
-                  <span className="icon-wrapper input-field-icon" onClick={handleDateIconClick}>
-                    <i className="fa-solid fa-calendar-days"></i>
-                  </span>
+          <div className="row field-group-wrapper">
+            <div className="col-12">
+              <p className="field-label">Schedule the game start date and time</p>
+            </div>
+            <div className="col-4">
+              <div className="form-block-wrapper">
+                <div className="custom-form-block">
+                  <label htmlFor="game-detail-date" className="form-label">
+                    Date
+                  </label>
+                  <div className="input-group date-field has-validation">
+                    <input type="date" name='start_date' id="date-copy" className='hide-input' onChange={handleDateChange} />
+                    <input
+                      type="text" 
+                      name="start_date"
+                      id='game-detail-date'
+                      className={`form-control ${(gameDetailForm.touched.start_date && gameDetailForm.errors.start_date) ? 'is-invalid' : ''}`}
+                      placeholder='Select Date'
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.start_date}
+                      readOnly/>
+                    <span className="icon-wrapper input-field-icon" onClick={handleDateIconClick}>
+                      <i className="fa-solid fa-calendar-days"></i>
+                    </span>
+                  </div>
+                  <div className="invalid-feedback">
+                    {
+                      gameDetailForm.errors.start_date && gameDetailForm.touched.start_date
+                      ? gameDetailForm.errors.start_date
+                      : null
+                    }
+                  </div>
+                </div>
+
+                <div className="custom-form-block">
+                  <label htmlFor="game-detail-time" className='form-label'>
+                    Time
+                  </label>
+                  <div className="input-group has-validation time-fields">
+                    <select
+                      name="start_time.hours"
+                      id="game-detail-time-hours"
+                      className={`form-select ${gameDetailForm.errors.start_time && gameDetailForm.touched.start_time ? 'is-invalid' : ''}`}
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.start_time.hours}>
+                      <option value="" selected disabled>Hours</option>
+                      {
+                        formOptionsData.start_time.hours.map((hr, hourIndex) => (
+                          <option value={hr} key={`hour-key-${hourIndex}`}>{hr}</option>
+                        ))
+                      }
+                    </select>
+
+                    <select
+                      name="start_time.minutes"
+                      id="game-detail-time-minutes"
+                      className={`form-select ${gameDetailForm.errors.start_time && gameDetailForm.touched.start_time ? 'is-invalid' : ''}`}
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.start_time.minutes}>
+                      <option value="" selected disabled>Min</option>
+                      {
+                        formOptionsData.start_time.minutes.map((min, minuteIndex) => (
+                          <option value={min} key={`minute-key-${minuteIndex}`}>{min}</option>
+                        ))
+                      }
+                    </select>
+
+                    <select
+                      name="start_time.meredian"
+                      id="game-detail-time-meredian"
+                      className={`form-select ${gameDetailForm.errors.start_time && gameDetailForm.touched.start_time ? 'is-invalid' : ''}`}
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.start_time.meredian}>
+                    {
+                      formOptionsData.start_time.meredian.map((mrd, meredianIndex) => (
+                        <option value={mrd} key={`meredian-key-${meredianIndex}`}>{mrd}</option>
+                      ))
+                    }
+                    </select>
+                  </div>
+                  <div className="invalid-feedback">
+                    {
+                      (gameDetailForm.errors.start_time && (gameDetailForm.touched.start_time))
+                      ? gameDetailForm.errors.start_time
+                      : null
+                    }
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="col-4">
+              <div className="form-block-wrapper">
+                <div className="custom-form-block time-zone-field">
+                  <label htmlFor="game-detail-time-zone" className="form-label">
+                    Time Zone
+                  </label>
+                  <div className="input-group has-validation">
+                    <select
+                      name="time_zone"
+                      id="game-detail-time-zone"
+                      className="form-select"
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.time_zone}>
+                    {
+                      formOptionsData.time_zome.map((zone, zoneIndex) => (
+                        <option value={zone} key={`time_zone-${zoneIndex}`}>{zone}</option>
+                      )) 
+                    }
+                    </select>
+                  </div>
+                  <div className="invalid-feedback">
+                    {
+                      gameDetailForm.errors.time_zone && gameDetailForm.touched.time_zone
+                      ? gameDetailForm.errors.time_zone
+                      : null
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row field-group-wrapper">
+            <div className="col-12">
+              <p className="field-label">"End User" details</p>
+            </div>
+            <div className="col-4">
+              <div className="form-block-wrapper">
+                <div className="custom-form-block">
+                  <label htmlFor="game-detail-user-name" className="form-label">Name *</label>
+                  <div className="input-group has-validation">
+                    <input 
+                      type="text"
+                      name="user_name"
+                      id="game-detail-user-name" 
+                      className={`form-control ${gameDetailForm.errors.user_name && gameDetailForm.touched.user_name ? 'is-invalid' : ''}`}
+                      placeholder={`End User's name`}
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.user_name}
+                    />
+                    <div className="invalid-feedback">
+                      {
+                        gameDetailForm.errors.user_name && gameDetailForm.touched.user_name
+                        ? gameDetailForm.errors.user_name
+                        : ''
+                      }
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="form-block-wrapper">
+                <div className="custom-form-block">
+                  <label htmlFor="game-detail-user-email" className="form-label">Email *</label>
+                  <div className="input-group has-validation">
+                    <input 
+                      type="text"
+                      name="user_email"
+                      id="game-detail-user-email" 
+                      className={`form-control ${gameDetailForm.errors.user_email && gameDetailForm.touched.user_email ? 'is-invalid' : ''}`}
+                      placeholder={`End User's Email`}
+                      onChange={gameDetailForm.handleChange}
+                      onBlur={gameDetailForm.handleBlur}
+                      value={gameDetailForm.values.user_email}
+                    />
+                    <div className="invalid-feedback">
+                      {
+                        gameDetailForm.errors.user_email && gameDetailForm.touched.user_email
+                        ? gameDetailForm.errors.user_email
+                        : ''
+                      }
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-4"></div>
           </div>
         </form>
       </div>
--- src/forms/game-detail-form.js
+++ src/forms/game-detail-form.js
@@ -1,19 +1,90 @@
 
 import { useFormik } from "formik";
-import * as Yup from "yup";
+// import * as Yup from "yup";
 
 const GameDetailForm = (formOptionsData) => {
+  console.log('Received formOptionsData ==> ', formOptionsData);
+
   const initialValues = {
-    start_date: ''
+    start_date: '',
+    start_time: {
+      hours: '',
+      minutes: '',
+      meredian: 'AM',
+    },
+    time_zone: 'UTC',
+    
+    user_name: '',
+    user_email: '',
+
+    cisco_name: '',
+    cisco_email: '',
+    cisco_title: '',
+
+
   }
 
-  const validationSchema = Yup.object().shape({
-    start_date: Yup.date().required('Required')
-  });
+  const validateDate = (given_date) => {
+    return /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/i.test(given_date);
+  }
+
+  const validate = (values) => {
+    let errors = {};
+
+    // Validating start_date
+    if (!values.start_date || !(validateDate(values.start_date.trim()))) {
+      errors.start_date = 'Select start date';
+    }
+
+    // Validating start_time
+    if (!(values.start_time.hours && values.start_time.minutes && values.start_time.meredian)) {
+      errors.start_time = 'Select valid start time';
+    }
+
+    // Validating time_zone
+    if (!values.time_zone || !formOptionsData.time_zome.includes(values.time_zome)) {
+      errors.time_zome = 'Select valid time zone';
+    }
+
+    // Validating user_name
+    if (!values.user_name || !values.user_name.trim()) {
+      errors.user_name = 'Enter valid name';
+    }
+
+    // Validating user_email
+    if (!values.user_email || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.user_email.trim())) {
+      errors.user_email = 'Enter valid email';
+    }
+
+    // Validating cisco_name
+    if (!values.cisco_name || !values.cisco_name.trim()) {
+      errors.cisco_name = 'Enter valid name';
+    }
+
+     // Validating cisco_email
+     if (!values.cisco_email || !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.cisco_email.trim())) {
+      errors.cisco_email = 'Enter valid email';
+    }
+
+    // Validating cisco_title
+    if (!values.cisco_title || !values.cisco_title.trim()) {
+      errors.cisco_title = 'Enter valid title';
+    }
+
+    return errors;
+  }
+
+  // const validationSchema = Yup.object().shape({
+  //   start_date: Yup.date().required('Required'),
+  //   hours: Yup.number().oneOf(formOptionsData.start_time.hours).required('Required'),
+  //   minutes: Yup.number().oneOf(formOptionsData.start_time.minutes).required('Required'),
+  //   meredian: Yup.string().oneOf(formOptionsData.start_time.meredian).required('Required'),
+  //   time_zone: Yup.string().oneOf(formOptionsData.time_zome).required('Required'),
+  // });
 
   return useFormik({
     initialValues,
-    validationSchema
+    validate,
   });
 }
 
